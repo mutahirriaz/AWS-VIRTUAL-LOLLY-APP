04446555de0834a8edd04dd20aacac114df921c0
mutahirriaz <64969144+mutahirriaz@users.noreply.github.com>
2021-02-21 19:48:38 +0500

    virtual-backend

--- virtual-lolly-backend/bin/virtual-lolly-backend.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { VirtualLollyBackendStack } from '../lib/virtual-lolly-backend-stack';
-
-const app = new cdk.App();
-new VirtualLollyBackendStack(app, 'VirtualLollyBackendStack');
--- virtuallolly-backend/test/virtuallolly-backend.test.ts
+++ virtuallolly-backend/test/virtuallolly-backend.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as VirtualLollyBackend from '../lib/virtual-lolly-backend-stack';
+import * as VirtuallollyBackend from '../lib/virtuallolly-backend-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new VirtualLollyBackend.VirtualLollyBackendStack(app, 'MyTestStack');
+    const stack = new VirtuallollyBackend.VirtuallollyBackendStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
